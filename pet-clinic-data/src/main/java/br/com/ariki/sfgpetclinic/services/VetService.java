2a812318159fba64dd8024b707cc0599475f482e
Daniel Ramon Ariki <dramonar@everis.com>
2019-04-11 13:44:52 -0300

    .

--- pet-clinic-data/src/main/java/br/com/ariki/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/br/com/ariki/sfgpetclinic/services/VetService.java
@@ -1,5 +1,7 @@
 package br.com.ariki.sfgpetclinic.services;
 
+import org.springframework.stereotype.Service;
+
 import br.com.ariki.sfgpetclinic.model.Vet;
 
 public interface VetService extends CrudService<Vet, Long> {
--- pet-clinic-web/src/main/java/br/com/ariki/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/br/com/ariki/sfgpetclinic/controller/VetController.java
@@ -5,12 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.ariki.sfgpetclinic.services.OwnerService;
+import br.com.ariki.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
 	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
