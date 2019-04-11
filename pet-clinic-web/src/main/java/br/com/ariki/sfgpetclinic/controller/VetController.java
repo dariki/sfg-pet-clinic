8d2fc11f100e0d7f9d10faf28ba3f0330641b20d
Daniel Ramon Ariki <dramonar@everis.com>
2019-04-11 13:38:59 -0300

    .

--- pet-clinic-web/src/main/java/br/com/ariki/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/br/com/ariki/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,16 @@
 package br.com.ariki.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.ariki.sfgpetclinic.services.OwnerService;
 
 @Controller
 public class VetController {
-
+	
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
 		return "vets/index";
 	}
 }
