82da304b37068349e61b5cc3b2914c5d77722f30
Daniel Ramon Ariki <dramonar@everis.com>
2019-04-11 15:41:03 -0300

    .

--- pet-clinic-data/src/main/java/br/com/ariki/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/br/com/ariki/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package br.com.ariki.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import br.com.ariki.sfgpetclinic.model.BaseEntity;
 
-	protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 	
 	Set<T> findAll() {
 		return new HashSet<>(this.map.values());
@@ -17,8 +20,9 @@ public abstract class AbstractMapService<T, ID> {
 		return this.map.get(id);
 	}
 	
-	T save(ID id, T object) {
-		this.map.put(id, object);
+	T save(T object) {
+		object.setId(this.getNextId());
+		this.map.put(object.getId(), object);
 		
 		return object;
 	}
@@ -29,5 +33,14 @@ public abstract class AbstractMapService<T, ID> {
 	
 	void delete(T object) {
 		this.map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	private Long getNextId() {
+		Long nextId = 0L;
+		
+		if(map.size() > 0) {
+			nextId = Collections.max(map.keySet());
+		}
+		
+		return nextId + 1;
 	}
 }
